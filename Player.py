dff3eb51d5002d9e83e2f0434442f75780676784
Ayoub21061 <21061@ecam.be>
2024-04-04 12:15:37 +0200

    Insertion du message dans le serveur + réecriture du code dans plusieurs fonctions

--- Player.py
+++ Player.py
@@ -1,5 +1,39 @@
 import socket
 import json
+import random
+import time
+
+def send_json_data(json_data, server_address):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(server_address)
+        json_string = json.dumps(json_data)
+        s.sendall(json_string.encode())
+        print("Données JSON envoyées au serveur avec succès.")
+        response = s.recv(20480)
+        print("Réponse du serveur:", response.decode())
+
+def handle_ping_pong():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 8887))
+        s.listen()
+        while True:
+            player, address = s.accept()
+            with player:
+                server_request = player.recv(2048).decode()
+                server_json = json.loads(server_request)
+                print("Requête du serveur:", server_json)
+                if server_json["request"] == "ping":
+                    response_pong = {"response": "pong"}
+                    response_pong_json = json.dumps(response_pong)
+                    player.sendall(response_pong_json.encode())
+                    print("Pong envoyé au serveur en réponse à la requête de ping.")
+                elif server_json["request"] == "play":
+                    response_move_string = {"response": "move", "move": "player_move", "message": "J'attends ton coup"}
+                    print(response_move_string)
+                    response_move_json = json.dumps(response_move_string)
+                    player.sendall(response_move_json.encode())
+                    print("Coup joué et réponse envoyée au serveur.")
+
 
 # Les données JSON que je dois envoyer
 json_data = {
@@ -9,39 +43,8 @@ json_data = {
     "matricules": ["21061", "68268"]
 }
 
+# Définir l'adresse IP et le port du serveur local
 server_address = ('localhost', 3000)
 
-# Création du socket pour la communication 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Établir la connexion au serveur
-        s.connect(server_address)
-
-        # J'envoie les données au serveur 
-        json_string = json.dumps(json_data)
-        s.sendall(json_string.encode())
-        print("Données JSON envoyées au serveur avec succès.")
-        
-        # Attendre la réponse du serveur
-        response = s.recv(2048)
-        
-        # Afficher la réponse du serveur
-        print("Réponse du serveur:", response.decode())
-
-
-# Opération du Ping-Pong
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('',8887)) 
-    s.listen()
-    while True:
-            player, adress = s.accept()
-            with player:
-                server_request = player.recv(2048).decode()
-                server_json = json.loads(server_request)
-        
-                # Vérifier si la requête est un ping
-                if server_json["request"] == "ping":
-                # Si c'est un ping, répondre avec un pong
-                    response_pong = {"response": "pong"}
-                    response_pong_json = json.dumps(response_pong)
-                    player.sendall(response_pong_json.encode())
-                    print("Pong envoyé au serveur en réponse à la requête de ping.")
+send_json_data(json_data, server_address)
+handle_ping_pong()
