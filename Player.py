eb73e01373b0aa96973fc4789565465d4a06af68
Ayoub21061 <21061@ecam.be>
2024-04-20 18:54:43 +0200

    Ajout des blockers

--- Player.py
+++ Player.py
@@ -2,6 +2,8 @@ import socket
 import json
 import random
 
+move_played = 0
+
 def send_json_data(json_data, server_address):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(server_address)
@@ -16,36 +18,157 @@ def player_mover(server_json):
     board = server_json["state"]["board"]
 
     player = server_json['state']['current'] # 0 ou 1
-    position = get_position(server_json, player) 
-    move_available = callfunction(board, position) # [True, True, False, True]
-    print(move_available)
-    #randommove = get_random_true_index(move_available)# 0 as True
-    listmoveavailable = get_list_position_true_index(move_available)
-    if player == 0:
-        if 3 in listmoveavailable:
-            return down_move(position)
-        elif 1 in listmoveavailable and 0 in listmoveavailable:
-            return random.choice([left_move(position), right_move(position)]) 
-        elif 1 in listmoveavailable:
-            return left_move(position)
-        elif 0 in listmoveavailable:
-            return right_move(position)
-        elif 2 in listmoveavailable:
-            return up_move(position)
+    position = get_position(server_json, player)
+    blockeravailable = server_json['state']['blockers'][player]
+    play = moveorblocker(blockeravailable)
+    if play == 'blocker':
+        opponent_player = get_opponent_player(player)
+        opponent_position = get_position(server_json, opponent_player)
+        if opponent_player == 0:
+            listblockeravailable = board[opponent_position[0] + 1]
+            if listblockeravailable[opponent_position[1]] == 3:
+                if opponent_position[1] < 2:
+                    if listblockeravailable[2] == 3:
+                        blockerlist = opponent_position[0] + 1 # [0,8] -> 0 -> 0 + 1 -> 1
+                        pos1 = 0
+                        pos2 = 2
+                        return play_blocker(blockerlist, pos1, pos2)
+                elif opponent_position[1] > 14:
+                    if listblockeravailable[14] == 3:
+                        blockerlist = opponent_position[0] + 1 # [0,8] -> 0 -> 0 + 1 -> 1
+                        pos1 = 14
+                        pos2 = 16
+                        return play_blocker(blockerlist, pos1, pos2)
+                else:
+                    leftorrightavailable = listposblockeravailable(listblockeravailable, opponent_position[1])
+                    true_indices = [i for i, val in enumerate(leftorrightavailable) if val]
+                    if len(true_indices) == 2:
+                        index = random.choice(true_indices)
+                    elif len(true_indices) == 1:
+                        index = true_indices[0]
+                    elif len(true_indices) == 0:
+                        play = 'move'
+
+                    if index == 0:
+                        pos1 = opponent_position[1] - 2 
+                        pos2 = opponent_position[1]
+                        blockerlist = opponent_position[0] + 1
+                        return play_blocker(blockerlist, pos1, pos2)
+                    elif index == 1:
+                        pos1 = opponent_position[1] + 2
+                        pos2 = opponent_position[1]
+                        blockerlist = opponent_position[0] + 1
+                        return play_blocker(blockerlist, pos1, pos2)
+            else:
+                play = 'move' 
+                print('Peut pas mettre de Blocker')       
+        else:
+            listblockeravailable = board[opponent_position[0] - 1]
+            if listblockeravailable[opponent_position[1]] == 3:
+                if opponent_position[1] < 2:
+                    if listblockeravailable[2] == 3:
+                        blockerlist = opponent_position[0] - 1 # [16,8] -> 16 -> 16 - 1 -> 15
+                        pos1 = 0
+                        pos2 = 2
+                        return play_blocker(blockerlist, pos1, pos2)
+                elif opponent_position[1] > 14:
+                    if listblockeravailable[14] == 3:
+                        blockerlist = opponent_position[0] - 1 # [0,8] -> 0 -> 0 + 1 -> 1
+                        pos1 = 14
+                        pos2 = 16
+                        return play_blocker(blockerlist, pos1, pos2)
+                else:
+                    leftorrightavailable = listposblockeravailable(listblockeravailable, opponent_position[1])
+                    true_indices = [i for i, val in enumerate(leftorrightavailable) if val]
+                    if len(true_indices) == 2:
+                        index = random.choice(true_indices)
+                    elif len(true_indices) == 1:
+                        index = true_indices[0]
+                    elif len(true_indices) == 0:
+                        play = 'move'
+                    if index == 0:
+                        pos1 = opponent_position[1] - 2 
+                        pos2 = opponent_position[1]
+                        blockerlist = opponent_position[0] - 1
+                        return play_blocker(blockerlist, pos1, pos2)
+                    elif index == 1:
+                        pos1 = opponent_position[1] + 2
+                        pos2 = opponent_position[1]
+                        blockerlist = opponent_position[0] - 1
+                        return play_blocker(blockerlist, pos1, pos2)
+            else:
+                play = 'move'
+                print('Peut pas mettre de Blocker')
+    if play == 'move':
+        print('On a move') 
+        move_available = callfunction(board, position) # [True, True, False, True]
+        #randommove = get_random_true_index(move_available)# 0 as True
+        listmoveavailable = get_list_position_true_index(move_available)
+        if player == 0:
+            if 3 in listmoveavailable:
+                return down_move(position)
+            elif 1 in listmoveavailable and 0 in listmoveavailable:
+                return random.choice([left_move(position), right_move(position)]) 
+            elif 1 in listmoveavailable:
+                return left_move(position)
+            elif 0 in listmoveavailable:
+                return right_move(position)
+            elif 2 in listmoveavailable:
+                return up_move(position)
+        else:
+            if 2 in listmoveavailable:
+                return up_move(position)
+            elif 1 in listmoveavailable and 0 in listmoveavailable:
+                return random.choice([left_move(position), right_move(position)]) 
+            elif 1 in listmoveavailable:
+                return left_move(position)
+            elif 0 in listmoveavailable:
+                return right_move(position)
+            elif 3 in listmoveavailable:
+                return down_move(position)
+
+def listposblockeravailable(listblockeravailable, posblocker):
+    result = []
+    result.append(blocker_left(listblockeravailable, posblocker))
+    result.append(blocker_right(listblockeravailable, posblocker))
+    return result
+
+def blocker_left(listeblockeravailable, posblocker):
+    if listeblockeravailable[posblocker - 2] == 3:
+        return True 
     else:
-        if 2 in listmoveavailable:
-            return up_move(position)
-        elif 1 in listmoveavailable and 0 in listmoveavailable:
-            return random.choice([left_move(position), right_move(position)]) 
-        elif 1 in listmoveavailable:
-            return left_move(position)
-        elif 0 in listmoveavailable:
-            return right_move(position)
-        elif 3 in listmoveavailable:
-            return down_move(position)
+        return False
 
-        
 
+def blocker_right(listeblockeravailable, posblocker):
+    if listeblockeravailable[posblocker + 2] == 3:
+        return True 
+    else:
+        return False
+    
+
+def play_blocker(blockerlist, pos1, pos2):
+    return {
+    "type": "blocker", 
+    "position": [[blockerlist,pos1], [blockerlist,pos2]]  
+}
+
+
+def get_opponent_player(player):
+    if player == 0:
+        return 1
+    else:
+        return 0   
+
+def moveorblocker(blockeravailable):
+    global move_played
+    move_played += 1
+    if blockeravailable == 0:
+        return 'move'
+    elif move_played % 2 == 0:
+        return 'blocker'
+    else:
+        return 'move'
 
 
 def right_move(position):
@@ -170,12 +293,13 @@ def handle_ping_pong():
                     state = server_json["state"]
                     errors = server_json["errors"]
                     player_move = player_mover(server_json)
+                    print(player_move)
                     response_move_string = {"response": "move", "move": player_move, "message": "Too far for Ronaldo to think about it"}
                     print(response_move_string)
                     response_move_json = json.dumps(response_move_string)
                     player.sendall(response_move_json.encode())
                     #print("Coup joué et réponse envoyée au serveur.")
-
+                    
 
 # Les données JSON que je dois envoyer
 json_data = {
